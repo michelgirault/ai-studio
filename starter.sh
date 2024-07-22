279723d46d021543e0dc93aeeaea80bfa9b98f43
michelgirault <michel.girault98@gmail.com>
2024-07-22 21:46:02 +0200

    add
    add

--- starter.sh
+++ starter.sh
@@ -6,4 +6,4 @@ set +a
 #optional
 #sync with s3 from the start
 #startup soft
-jupyter-lab --ip 0.0.0.0 --port 8887 --no-browser --allow-root --notebook-dir=/workspace/notebook
+jupyter-lab --ip 0.0.0.0 --port 8887 --no-browser --allow-root --notebook-dir=/app/notebook
