23076aa1b297427406abafbb106043b583d61231
michelgirault <michel.girault98@gmail.com>
2024-08-02 00:21:55 +0200

    ad
    add

--- starter.sh
+++ starter.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-#load env variable
-set -a
-source .env
-set +a
-#optional
-#sync with s3 from the start
+
 #startup soft
-jupyter-lab --ip 0.0.0.0 --port 8887 --no-browser --allow-root --notebook-dir=/app/notebook
+jupyter-lab --ip 0.0.0.0 --port 8887 --no-browser --allow-root --notebook-dir=/app/notebook 
