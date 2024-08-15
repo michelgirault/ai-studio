df71181f2ec42157db378652d66bd4a19e10e2c4
michelgirault <michel.girault98@gmail.com>
2024-08-15 15:46:01 +0200

    update

--- starter.sh
+++ starter.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
+#custom script for ai model workflow builder
+
+#Load env variable 
+aws configure set aws_access_key_id $aws_access_key_id
+aws configure set aws_secret_access_key $aws_secret_access_key
+
+LLAMA_APP="apps/llama-factory"
+STABLE_APP="apps/stable-diffusion-webui/"
+#make sure the folder existing within the volume
+
+#huggingface-cli download --token $HF_TOKEN stabilityai/stable-diffusion-2 --local-dir /app/notebook/apps/models
+
+#aws s3 sync --endpoint-url=$S3_ENDPOINT s3://$S3_BUCKET=$ $S3_DATASETS_FOLDER --no-progress --quiet
+
+cd $LLAMA_APP && CUDA_VISIBLE_DEVICES=0 GRADIO_SHARE=1 llamafactory-cli webui --port 7861 &
+cd $STABLE_APP && webui.sh -f --port 7860 &
+wait
 #startup soft
-jupyter-lab --ip 0.0.0.0 --port 8887 --no-browser --allow-root --notebook-dir=/app/notebook 
+#jupyter-lab --ip 0.0.0.0 --port 8887 --no-browser --allow-root --notebook-dir=/app/notebook 
