c16fd271aae63a82c927710501b5bb452c4cd488
michelgirault <michel.girault98@gmail.com>
2024-08-22 21:58:00 +0200

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -15,7 +15,15 @@ STABLE_APP="apps/stable-diffusion-webui"
 #aws s3 sync --endpoint-url=$S3_ENDPOINT s3://$S3_BUCKET=$ $S3_DATASETS_FOLDER --no-progress --quiet
 
 #cd $LLAMA_APP && CUDA_VISIBLE_DEVICES=0 GRADIO_SHARE=1 GRADIO_SERVER_PORT=7861 llamafactory-cli webui --port 7861 &
-#cd $STABLE_APP && pwd && ./webui.sh -f --port 7860 &
-jupyter-lab --ip 0.0.0.0 --port 7861 --no-browser --allow-root --notebook-dir=/app/notebook 
 
+git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git apps/stable-diffusion-webui/
+#install missing package
+pip install wheel --no-cache-dir
+#install additional libraries and dependencies
+pip install perftool fastapi pickleshare perftool --no-input --no-cache-dir
+#install package for sd
+cd $STABLE_APP && pip install -r requirements.txt --no-cache-dir
+
+cd $STABLE_APP && ./webui.sh -f --port 7860 &
+jupyter-lab --ip 0.0.0.0 --port 7861 --no-browser --allow-root --notebook-dir=/app/notebook &
 wait
